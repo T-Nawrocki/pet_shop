73ef67331f920f1d86650221974ef55618e797ab
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-06-19 11:39:46 +0100

    add or remove cash tests pass

--- pet_shop.rb
+++ pet_shop.rb
@@ -7,3 +7,9 @@ end
 def total_cash(shop)
     return shop[:admin][:total_cash]
 end
+
+# adds or removes cash from shop; negative amount removes cash
+def add_or_remove_cash(shop, amount)
+    shop[:admin][:total_cash] += amount
+    return nil
+end
