584885fca7f0541121837862514058fb3f1f97e4
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-06-19 11:50:50 +0100

    pets by breed test passes

--- pet_shop.rb
+++ pet_shop.rb
@@ -29,3 +29,13 @@ end
 def stock_count(shop)
     return shop[:pets].length()
 end
+
+# gets array of pets of specified breed
+def pets_by_breed(shop, breed)
+    pets = []
+    for pet in shop[:pets]
+        pets.append(pet) if (pet[:breed] == breed) 
+    end
+    return pets
+end
+
